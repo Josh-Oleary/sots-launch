f8f1da32378b2172a03b36132bcf160facfc9bad
Josh <jdoleary91@gmail.com>
2021-11-29 09:48:05 -0500

    refactor: seperate route logic to controllers folder

--- sots/routes/users.js
+++ sots/routes/users.js
@@ -1,64 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
-const isLoggedIn = require('../middleware');
-const bcrypt = require('bcrypt');
+const users = require('../controllers/users')
 
+router.route('/register')
+    .get(users.renderRegister)
+    .post(users.registerUser);
 
-router.get('/register', (req, res) => {
-    res.render('users/register');
-});
-//register user w/ passport
-router.post('/register', async (req, res) => {
-    try {
-        const { email, password, name } = req.body;
-        const hashPass = await bcrypt.hash(password, 10)
-        const user = new User({ 
-            email: email,
-            password: hashPass,
-            name: name,
-        });
-        const registeredUser = await user.save(user);
-        req.login(registeredUser, err => {
-            if(err) return next(err);
-            req.flash('success', 'Welcome to SOTS');
-            res.redirect('/');
-        })
-    } catch (e) {
-        console.log(e)
-        req.flash('error', e.message)
-        res.redirect('register')
-    }
-});
-router.get('/login', (req, res) => {
-    res.render('users/login');
-})
+router.route('/login')
+    .get(users.renderLogin)
+    .post(users.loginUser)
 
-// login/logout routes using bcrypt
-router.post('/login', async (req, res) => {
-    try {
-        const user = await User.findOne({ email: req.body.email })
-        console.log('User: ', user)
-        if(user){
-            const cmp = await bcrypt.compare(req.body.password, user.password);
-            if(cmp){
-                req.login(user, err => {
-                    if(err) return next(err);
-                    req.flash('success', `Welcome back, ${user.name}!`);
-                    res.redirect('/');
-                })
-            }
-        }
-    } catch (e) {
-        console.log('Error: ', e);
-    }
-}
-);
+router.route('/logout')
+    .get(users.logout)
 
-router.get('/logout', (req, res) => {
-    req.logout();
-    console.log('logged out');
-    req.flash('success', 'Goodbye!');
-    res.redirect('/');
-})
 module.exports = router;
