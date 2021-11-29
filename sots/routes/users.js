369307880528793c30c5f3abc621f97b5595b92b
Josh <jdoleary91@gmail.com>
2021-11-29 09:10:42 -0500

    remove old packages

--- sots/routes/users.js
+++ sots/routes/users.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
-const isLoggedIn = require('../middleware');
 const bcrypt = require('bcrypt');
 
 
