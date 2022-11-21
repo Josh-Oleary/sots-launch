1a6626c750b6d1b620f65a481a99fb78bd221383
Josh O <josh.oleary@gaiaherbs.com>
2022-11-21 08:18:10 -0600

    update

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET
+    cloud_name: 'ddfdmqleo',
+    api_key: 594265469115286,
+    api_secret: 'xeVWIPubJ0uL2Xd2UBoVvBnSOXY'
 });
 
 const storage = new CloudinaryStorage({
